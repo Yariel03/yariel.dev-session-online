6e3398dd4ba70b58f736fed5651986d446703f41
Yariel Baldeon <bryan.baldeon@espoch.edu.ec>
2023-02-01 14:46:52 -0500

    lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,1 @@
-import { Isession } from "./interface/index";
-/**Class Session */
-export declare class Sessiones implements Isession {
-    /**Setup
-     * @class Session - Clase para manejar las sesiones
-     * @method setup - Metodo para configurar las rutas de la clase
-     * @param app instancia de express obligatorio
-     * @param server instancia del server levantado en http (opcional) solo si desea usar socket.io
-     */
-    setup: (app: any, server?: undefined) => void;
-}
+export {};
